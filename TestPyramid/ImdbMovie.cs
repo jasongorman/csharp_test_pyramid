1a63ffa221d9af34439f3676d9a8c3d3906248d4
Jason Gorman <jason.gorman@codemanship.com>
2020-08-26 12:03:11 +0100

    Moved ImdbMovie data class to own file

---
+++ TestPyramid/ImdbMovie.cs
@@ -0,0 +1,16 @@
+﻿namespace TestPyramid
+{
+    internal class ImdbMovie
+    {
+        public string Title {
+            get;
+            set;
+        }
+
+        public string imdbRating
+        {
+            get;
+            set;
+        }
+    }
+}
--- TestPyramid/Pricer.cs
+++ TestPyramid/Pricer.cs
@@ -37,18 +37,4 @@ namespace TestPyramid
             return new Movie(imdbMovie.Title, price);
         }
     }
-
-    internal class ImdbMovie
-    {
-        public string Title {
-            get;
-            set;
-        }
-
-        public string imdbRating
-        {
-            get;
-            set;
-        }
-    }
 }
